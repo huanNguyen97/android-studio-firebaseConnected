5ec42a2b32d09bfc5f41857e3e45d08161b7c8ad
nguye <nguyencaohuan1997@gmail.com>
2022-02-26 15:55:23 +0700

    OK

--- app/src/main/java/com/example/firebasetesting1/MainActivity.java
+++ app/src/main/java/com/example/firebasetesting1/MainActivity.java
@@ -1,23 +1,64 @@
 package com.example.firebasetesting1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference();
 
-        myRef.setValue("Hello, World!");
+        // set new value database
+        myRef.child("users3").child("ok").setValue("Hello HUan");
+        // End
+
+        // Read database (only one value)
+        myRef.child("user/1/name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String age = snapshot.getValue(String.class);
+                textView.setText(age);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // Use for an array
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 }
